7e5f42f737651b29bb531564e472a0dd2e4abc8e
Abdullah Al Shahed <abdullahalshahedkhan@gmail.com>
2025-06-26 09:06:23 +0600

    user import model and rule addedd

--- app/Imports/UserImport.php
+++ app/Imports/UserImport.php
@@ -3,9 +3,12 @@
 namespace App\Imports;
 
 use App\Models\User;
+use Illuminate\Support\Facades\Hash;
 use Maatwebsite\Excel\Concerns\ToModel;
+use Maatwebsite\Excel\Concerns\WithHeadingRow;
+use Maatwebsite\Excel\Concerns\WithValidation;
 
-class UserImport implements ToModel
+class UserImport implements ToModel, WithHeadingRow, WithValidation
 {
     /**
     * @param array $row
@@ -15,7 +18,18 @@ class UserImport implements ToModel
     public function model(array $row)
     {
         return new User([
-            //
+            'name' => $row['name'],
+            'email' => $row['email'],
+            'password' => Hash::make($row['password']),
         ]);
+    }
+
+    public function rules(): array
+    {
+        return [
+            'name' => 'required|string|max:255',
+            'email' => 'required|email|unique:users,email',
+            'password' => 'required|string|min:8',
+        ];
     }
 }
